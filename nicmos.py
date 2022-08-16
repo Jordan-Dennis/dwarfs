d831eb05d211227753320112928ffb87b8bd466b
Jordan Dennis <jdenn105@gmail.com>
2022-08-16 14:14:02 +1000

    Faulty implementation of the nicmos model that used multiplication instead of binary mask indexing

--- nicmos.py
+++ nicmos.py
@@ -12,19 +12,45 @@ _radial = lambda npix : (_cartesian(npix) ** 2).sum(axis = 0)
 
 
 def nicmos(npix: int, radius: int) -> Matrix:
+    """
+    References:
+    -----------
+    1. Observatory - Optics, NASA, May 27, 2022,
+       https://www.nasa.gov/content/goddard/
+       hubble-space-telescope-optics-system
+    """
     pixel_scale = 2 * radius / npix 
     cartesian = _cartesian(npix) * pixel_scale
     radial = _radial(npix) * pixel_scale
+   
+    # The optical telescope assembly refers to the optical system 
+    # frame (NASA, 2022). 
+    optical_telescope_assembly =\
+        ((radial <= radius) && (radial >= .33))\
+        .at[:]\
+        .mul((cartesian > .011 * radius).sum(axis = 0))\
+        .at[:]\
+        .mul(np.roll(radial, int(.89221 * radius), axis=1)\
+            >= .065 * radius)\
+        .at[:]\
+        .mul(np.roll(
+            np.roll(radial, int(.7555 * radius), axis=0), 
+            int(-.4615 * radius), axis=1) >= .065 * radius)
+
+    # TODO: Remember that I have not copied exactly and I am 
+    # multiplying but I should be using .set so that I can have 
+    # it exactly the same.
+    # TODO: I can use boolean masking and make this exactly the 
+    # same which will be the way that I do this moving forward.
+    nicmos_cold_mask =\
+        (radial <= .955 * radius)\
+        .at[:]\
+        .mul(radial <= .372 * radius)\
+        .at[:]\
+        .mul(np.abs(cartesian[0]) > .0335 * radius)\
+        .at[:]\
+        .mul(np.abs(cartesian[1]) > .0335 * radius)\
+
+
     
-    nicmos = (radial <= radius) && (radial >= .33)
-    nicmos = nicmos * (cartesian > .011 * radius).sum(axis = 0)
-    
-    # TODO: Work out what the fuck this is for.
-    temporary = np.roll(radial, int(.89221 * radius), axis=1)
-    nicmos = nicmos * (temporary >= .065 * radius)
-    
-    temporary = np.roll(
-        np.roll(radial, int(.7555 * radius), axis=0), 
-        int(-.4615 * radius), axis=1)
-    nicmos = nicmos * (temporary >= .065 * radius)
     
