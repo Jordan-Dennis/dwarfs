7295931bb6d8d52d3b5881c66d0f4fc0867ba4f0
Jordan Dennis <jdenn105@gmail.com>
2022-08-16 15:06:17 +1000

    Using .at[boolean_mask] and vastly improving the readability of the code

--- nicmos.py
+++ nicmos.py
@@ -25,23 +25,42 @@ def nicmos(npix: int, radius: int) -> Matrix:
    
     # The optical telescope assembly refers to the optical system 
     # frame (NASA, 2022). 
-    optical_telescope_assembly =\
-        ((radial <= radius) && (radial >= .33))\
-        .at[:]\
-        .mul((cartesian > .011 * radius).sum(axis = 0))\
-        .at[:]\
-        .mul(np.roll(radial, int(.89221 * radius), axis=1)\
-            >= .065 * radius)\
-        .at[:]\
-        .mul(np.roll(
-            np.roll(radial, int(.7555 * radius), axis=0), 
-            int(-.4615 * radius), axis=1) >= .065 * radius)
+    mirror_pad_edges = (
+        (int(.89221 * radius),), 
+        (int(.7555 * radius), int(-.4615 * radius)),
+        (int(-.7606 * radius), int(-.4615 * radius)))
+    mirror_pad_outer_edge = .065 * radius
+
+    outer_radius = (radial <= radius)
+    telescope_obstruction = (radial <= .33 * radius)
+    horizontal_spider = (np.abs(cartesian[0]) < .011 * radius)
+    vertical_spider = (np.abs(cartesian[1]) < .011 * radius)
+    mirror_pad = (np.roll(radial, mirror_pad_edges[0], axis=(1,))\
+            <= mirror_pad_outer_edge)\
+        && (np.roll(radial, mirror_pad_edges[1], axis=(0, 1))\
+            <= mirror_pad_outer_edge)\
+        && (np.roll(radial, mirror_pad_edges[2], axis=(0, 1))\
+            <= mirror_pad_outer_edge)
+
+    optical_telescope_assembly = np.zeros_like(radial)\
+        .at[outer_radius].set(1.)\
+        .at[telescope_obstruction].set(0.)\
+        .at[horizontal_spider].set(0.)\
+        .at[vertical_spider].set(0.)\
+        .at[mirror_pad].set(0.)
+
 
     # TODO: Remember that I have not copied exactly and I am 
     # multiplying but I should be using .set so that I can have 
     # it exactly the same.
     # TODO: I can use boolean masking and make this exactly the 
     # same which will be the way that I do this moving forward.
+    outer_radius = (radial <= .955 * radius)
+    obstruction = (radial <= .372 * radius)
+    vertical_spider = (np.abs(cartesian[0]) < .0335 * radius)
+    horizontal_spider = (np.abs(cartesian[1]) < .0335 * radius)
+    mirror_pads = 
+
     nicmos_cold_mask =\
         (radial <= .955 * radius)\
         .at[:]\
