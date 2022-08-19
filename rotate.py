c3fc62869937b8a9e6c953faed58528b0b71e7ee
Jordan Dennis <jdenn105@gmail.com>
2022-08-19 14:43:58 +1000

    A first attempt at the rotation in JAX with properly self documenting code. There is a good chance that I have broken things doing this

--- rotate.py
+++ rotate.py
@@ -1,28 +1,36 @@
 import jax.numpy as np
 from scipy import fftpack
+from jax.scipy.ndimage import map_coordinates
+from dLux.utils import polar2cart, cart2polar, get_pixel_positions
+from typing import TypeVar
+
+matrix = TypeVar("matrix")
 
 
-def _cartesian_to_polar(x: matrix, y: matrix)
-
-
-def _rotate(image: matrix, rotation: float):
+def rotate(
+        image: matrix, 
+        rotation: float):
     npix = image.shape[0]
     centre = (npix - 1) / 2
     x_pixels, y_pixels = get_pixel_positions(npix)
     rs, phis = cart2polar(x_pixels, y_pixels)
     phis += rotation
-    coordinates_rot = np.roll(polar2cart(rs, phis) + centre, shift=1, axis=0)
-    rotated = map_coordinates(array, coordinates_rot, order=order)
+    coordinates_rot = np.roll(polar2cart(rs, phis) + centre, 
+        shift=1, axis=0)
+    rotated = map_coordinates(array, coordinates_rot, order=1)
     return rotated
 
 
-def fft_3shear_rotate_pad(in_frame, alpha, pad=4, return_full=False):
+def conserve_information_and_rotate(
+        image: matrix, 
+        alpha: float, 
+        pad: int = 4) -> matrix:
 	# We need to add some extra rows since np.rot90 has a different definition of the centre
-    in_shape = in_frame.shape
+    in_shape = image.shape
     image_shape = map(lambda x: x + 3, in_shape)
 	image = np.full(shape, np.nan, dtype=float)\
         .at[1 : in_shape[0] + 1, 1 : in_shape[1] + 1]\
-        .set(in_frame)
+        .set(image)
 
 	# FFT rotation only work in the -45:+45 range
     # So I need to work out how to determine the quadrant that alpha is in and hence the 
@@ -49,42 +57,43 @@ def fft_3shear_rotate_pad(in_frame, alpha, pad=4, return_full=False):
         .at[left_corner : right_corner, top_corner : bottom_corner]\
         .set(image)
 
-	pad_mask = np.ones(out_shape, dtype=bool)
-	pad_mask[px1:px2,py1:py2]=np.where(np.isnan(in_frame),True,False)
+	padded_mask = np.ones(out_shape, dtype=bool)\
+        .at[left_corner : right_corner, top_corner : bottom_corner]\
+        .set(np.where(np.isnan(image), True, False))
 	
 	# Rotate the mask, to know what part is actually the image
-	pad_mask=ndimage.interpolation.rotate(pad_mask, np.rad2deg(-alpha_rad),
-		  reshape=False, order=0, mode='constant', cval=True, prefilter=False)
+	padded_mask = rotate(padded_mask, -angle_in_first_quadrant)
 
 	# Replace part outside the image which are NaN by 0, and go into Fourier space.
-	pad_frame=np.where(np.isnan(pad_frame),0.,pad_frame)
+	padded_frame = np.where(np.isnan(padded_image), 0. , padded_image)
 
+	uncentered_angular_displacement = np.tan(angle_in_first_quadrant / 2.)
+	centered_angular_displacement = -np.sin(angle_in_first_quadrant)
 
-	###############################
-	# Rotation in Fourier space
-	###############################
-	a=np.tan(alpha_rad/2.)
-	b=-np.sin(alpha_rad)
+	uncentered_frequencies = np.fft.fftfreq(out_shape[0])
+    centered_frequencies = np.arange(-out_shape[0] / 2., out_shape[0] / 2.)
 
-	M=-2j*np.pi*np.ones(pad_frame.shape)
-	N=fftpack.fftfreq(pad_frame.shape[0])
+    uncentered_phase = np.exp(
+        uncentered_angular_displacement *\
+        ((-2.j * np.pi * uncentered_frequencies).T *\
+        centered_frequecies).T)).T
+    centered_phase = np.exp(
+        centered_angular_displacement *\
+        ((-2.j * np.pi * centered_frequencies).T *\
+        uncentered_frequencies)
 
-	X=np.arange(-pad_frame.shape[0]/2.,pad_frame.shape[0]/2.)#/pad_frame.shape[0]
+    rotated_image = np.fft.ifft(
+        np.fft.fft(
+            np.fft.ifft(
+                np.fft.fft(
+                    np.fft.ifft(
+                        np.fft.fft(padded_image, axis=0).T * uncentered_phase, 
+                        axis=0), 
+                    axis=1) * centered_phase, 
+                axis=1), 
+            axis=0).T * uncentered_phase, 
+        axis=0)\
+        .at[padded_mask]\
+        .set(np.nan)
 
-	pad_x=fftpack.ifft((fftpack.fft(pad_frame, axis=0,overwrite_x=True).T*
-		np.exp(a*((M*N).T*X).T)).T, axis=0,overwrite_x=True)
-	pad_xy=fftpack.ifft(fftpack.fft(pad_x,axis=1,overwrite_x=True)*
-		np.exp(b*(M*X).T*N), axis=1,overwrite_x=True)
-	pad_xyx=fftpack.ifft((fftpack.fft(pad_xy, axis=0,overwrite_x=True).T*
-		np.exp(a*((M*N).T*X).T)).T,axis=0,overwrite_x=True)
-
-	# Go back to real space
-	# Put back to NaN pixels outside the image.
-
-	pad_xyx[pad_mask]=np.NaN
-
-
-	if return_full:
-		return np.real(pad_xyx).copy()
-	else:
-		return np.real(pad_xyx[px1:px2,py1:py2]).copy()
+    return np.real(rotated_image).copy()
