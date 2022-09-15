28653bd87215a6573de3b73341f227667674d6a1
Jordan Dennis <jdenn105@gmail.com>
2022-09-15 10:45:40 +1000

    Complete documentation for the Spider classes

--- spider.py
+++ spider.py
@@ -4,7 +4,6 @@ import typing
 import equinox
 import functools
 import dLux.utils 
-#import numpy as np
 import jax.numpy as np 
 import matplotlib.pyplot as pyplot
 
@@ -110,8 +109,6 @@ class Spider(equinox.Module, abc.ABC):
         return np.apply_along_axis(np.matmul, 0, coordinates, rotation_matrix) 
 
 
-    # TODO: This needs to be truncated to the radius of the spider. 
-    # @functools.partial(jax.vmap, in_axes=(None, 0, None))
     def _strut(self: Layer, angle: float, width: float) -> float:
         """
         Generates a representation of a single strut in the spider. This is 
@@ -145,6 +142,23 @@ class Spider(equinox.Module, abc.ABC):
         """
         The name is a misnomer but it has been kept for legacy reasons. 
         This is a routine that is used in the soft edging of the images. 
+        
+        Parameters
+        ----------
+        distance: float, meters
+            A matrix representing the distance of each pixel from a point 
+            line or otherwise a shape. Some useful hints for using this are 
+            that setting regions to np.inf will convert them into exactly 1.
+            i.e. inside a box. 
+        width: float, meters
+            This roughly represents the amount of soft edging. The best way to 
+            use this parameter is by trying a few different values until you 
+            find one that you like.
+
+        Returns
+        -------
+        image: float 
+            The soft edged image, taken from distance. 
         """
         steepness = self.number_of_pixels
         return (np.tanh(steepness * (distance - width)) + 1.) / 2.
@@ -152,15 +166,52 @@ class Spider(equinox.Module, abc.ABC):
 
     @abc.abstractmethod
     def _spider(self: Layer) -> float:
+        """
+        Represent the spider as an number_of_pixels by umber_of_pixels array. 
+
+        Returns 
+        -------
+        spider: float
+            The soft edged array representation of the spider. 
+        """
         pass 
 
     
     @abc.abstractmethod
     def __call__(self: Layer, params: dict) -> dict:
+        """
+        Apply the spider to a wavefront. 
+
+        Parameters
+        ----------
+        params: dict
+            A dictionary of parameters that must contain a "Wavefront" entry.
+        
+        Returns 
+        -------
+        params: dict
+            The same dictionary of parameter with an updated "Wavefront" entry.
+        """
         pass 
 
 
 class UniformSpider(Spider):
+    """
+    A spider with equally-spaced, equal-width struts. This is of course the 
+    most common and simplest implementation of a spider. Gradients can be 
+    taken with respect to the width of the struts and the global rotation 
+    as well as the centre of the spider inherited from 
+
+    Parameters
+    ----------
+    number_of_struts: int 
+        The number of struts to equally space around the circle. This is not 
+        a differentiable parameter. 
+    width_of_struts: float, meters
+        The width of each strut. 
+    rotation: float, radians
+        A global rotation to apply to the entire spider. 
+    """
     number_of_struts: int
     width_of_struts: float
     rotation: float
@@ -175,6 +226,29 @@ class UniformSpider(Spider):
             number_of_struts: int, 
             width_of_struts: float, 
             rotation: float) -> Layer:
+        """
+        Parameters
+        ----------
+        number_of_pixels: int
+            The number of pixels along one edge of the image used to represent 
+            the spider. 
+        radius_of_spider: float, meters
+            The physical width of the spider. For the moment it is assumed to 
+            be embedded within a circular aperture.         
+        width_of_image: float, meters
+            The width of the image. If you wish to pad the array representation 
+            of the spider then set this to the padding factor multiplied by the 
+            radius_of_spider parameter. 
+        center_of_spicer: Array, meters 
+            The [x, y] center of the spider.
+        number_of_struts: int 
+            The number of struts to equally space around the circle. This is not 
+            a differentiable parameter. 
+        width_of_struts: float, meters
+            The width of each strut. 
+        rotation: float, radians
+            A global rotation to apply to the entire spider.
+        """ 
         super().__init__(
             width_of_image, 
             number_of_pixels, 
@@ -185,16 +259,46 @@ class UniformSpider(Spider):
         self.width_of_struts = np.asarray(width_of_struts).astype(float)
 
 
+    @functools.partial(jax.vmap, in_axes=(None, 0, None))
+    def _strut(self: Layer, angle: float, width: float) -> float:
+        """
+        A vectorised routine for constructing the struts. This has been done 
+        to improve the performance of the program, and simply differs to 
+        the super-class implementation. 
+
+        Parameters
+        ----------
+        angle: float, radians
+            The angle that this strut points as measured from the positive 
+            x-axis in radians. 
+        width: float, meters
+            The width of the strut in meters.
+
+        Returns
+        -------
+        strut: float
+            The soft edged strut.
+        """
+        return super()._strut(angle, width)
+
+
     def _spider(self: Layer) -> float:
+        """
+        Represents the spider in a square array. Each strut is placed equally 
+        around the circumference like a wll cut pizza. All the struts have the
+        same width and a global rotation.
+    
+        Returns
+        -------
+        spider: float
+            The array representation of the spider. 
+        """
         angles = np.linspace(0, 2 * np.pi, self.number_of_struts, 
             endpoint=False)
         angles += self.rotation
 
-        spider = np.ones((self.number_of_pixels, self.number_of_pixels))
-        for angle in angles:
-            spider *= self._strut(angle, self.width_of_struts)
+         spider = np.prod(self._strut(angle, self.width_of_struts), axis=0)
 
-        # TODO: This needs to be moved to the outside. 
         coordinates = self._coordinates()
         radial_coordinates = np.hypot(coordinates[0], coordinates[1])
 
@@ -206,10 +310,22 @@ class UniformSpider(Spider):
         radial_soft_edge = self._sigmoid(radial_distance, soft_edge)
 
         return radial_soft_edge * spider
-        # return self._strut(angles).sum(axis=0) / self.number_of_struts
         
  
     def __call__(self: Layer, params: dict) -> dict:
+        """
+        Apply the spider to a wavefront, as it propagates through the spider. 
+
+        Parameters
+        ----------
+        params: dict
+            A dictionary of parameters that contains a "Wavefront" key. 
+
+        Returns 
+        -------
+        params: dict 
+            The same dictionary with the "Wavefront" value updated.
+        """
         aperture = self._spider()
         wavefront = params["Wavefront"]
         wavefront = wavefront\
