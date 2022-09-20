30e5c8c53d3de6e8870867420e33b89e0d08c724
Jordan Dennis <jdenn105@gmail.com>
2022-09-21 09:22:21 +1000

    Complete aperture

--- hubble.py
+++ hubble.py
@@ -6,20 +6,20 @@ import jax.numpy as np
 
 apertures = {
         "1": dLux.CircularAperture( 
-            x_offset = 1.070652, 
-            y_offset = 0.0,
+            x_offset = 1.070652 * np.cos(- np.pi / 4), 
+            y_offset = 1.070652 * np.sin(- np.pi / 4),
             radius = 0.078,
             occulting = True,
             softening = True),
         "2": dLux.CircularAperture(
-            x_offset = -0.5538,
-            y_offset = 0.9066,
+            x_offset = 1.070652 * np.cos(-np.pi / 4 + 2 * np.pi / 3), 
+            y_offset = 1.070652 * np.sin(-np.pi / 4 + 2 * np.pi / 3),
             radius = 0.078,
             occulting = True,
             softening = False),
         "3": dLux.CircularAperture(
-            x_offset = -0.54768,
-            y_offset = -0.91272,
+            x_offset = 1.070652 * np.cos(-np.pi / 4 - 2 * np.pi / 3), 
+            y_offset = 1.070652 * np.sin(-np.pi / 4 - 2 * np.pi / 3),
             radius = 0.078,
             occulting = True,
             softening = False),
@@ -44,43 +44,43 @@ apertures = {
             softening = True),
         # NOTE: Below this is the cold mask
         "12": dLux.CircularAperture(
-            x_offset = 0.096,
-            y_offset = 0.0,
+            x_offset = - 0.0678822510,
+            y_offset = - 0.0678822510,
             radius = 1.2,
             occulting = False,
             softening = True),
         "6": dLux.CircularAperture(
-            x_offset = 0.096,
-            y_offset = 0.0,
+            x_offset = - 0.0678822510,
+            y_offset = - 0.0678822510,
             radius = 0.4464,
             occulting = True,
             softening = True),
         "7": dLux.EvenUniformSpider(
-            x_offset = -0.096,  
-            y_offset = 0.0,
+            x_offset = - 0.0678822510,
+            y_offset = - 0.0678822510,
             number_of_struts = 4,
             width_of_struts = 0.0402,
             rotation = 0.785398163,
             softening = True),
         "8": dLux.SquareAperture(
-            x_offset = -0.478775884, 
-            y_offset = -0.781291236,
-            theta = -2.12057504,
-            width = 0.078,
+            x_offset = 1.070652 * np.cos(- np.pi / 4) - 0.0678822510, 
+            y_offset = 1.070652 * np.sin(- np.pi / 4) - 0.0678822510,
+            theta = - np.pi / 4,
+            width = 0.156,
             occulting = True,
             softening = True),
         "9": dLux.SquareAperture(
-            x_offset = 0.91632, 
-            y_offset = 0.0,
-            theta = 0.0,
-            width = 0.078,
+            x_offset = 1.070652 * np.cos(- np.pi / 4 + 2 * np.pi / 3) - 0.0678822510, 
+            y_offset = 1.070652 * np.sin(- np.pi / 4 + 2 * np.pi / 3) - 0.0678822510,
+            theta = - np.pi / 4 + np.pi / 3,
+            width = 0.156,
             occulting = True, 
             softening = True),
         "10": dLux.SquareAperture(
-            x_offset = -0.471396840, 
-            y_offset = 0.785559492,
-            theta = 2.11184839,
-            width = 0.078,
+            x_offset = 1.070652 * np.cos(- np.pi / 4 - 2 * np.pi / 3) - 0.0678822510, 
+            y_offset = 1.070652 * np.sin(- np.pi / 4 - 2 * np.pi / 3) - 0.0678822510,
+            theta = - np.pi / 3 - np.pi / 4,
+            width = 0.156,
             occulting = True,
             softening = True)}
 
@@ -88,9 +88,10 @@ nicmos = dLux.CompoundAperture(apertures)
 
 coordinates = dLux.utils.get_pixel_coordinates(1024, 0.003, 0., 0.)
 
-for i, aperture in enumerate(apertures.values()):
+for i, aperture in enumerate(apertures):
     pyplot.subplot(3, 4, i + 1)
-    pyplot.imshow(aperture._aperture(coordinates))
+    pyplot.title(aperture)
+    pyplot.imshow(apertures[aperture]._aperture(coordinates))
     pyplot.colorbar()
 pyplot.show() 
 
