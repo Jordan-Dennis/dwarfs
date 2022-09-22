7e72c755d6af2092238825765208d2f3d416dbeb
Jordan Dennis <jdenn105@gmail.com>
2022-09-23 08:09:21 +1000

    Creating a translatable NicmosColdMask class

--- .ipynb_checkpoints/hubble-checkpoint.py
+++ .ipynb_checkpoints/hubble-checkpoint.py
@@ -1,10 +1,83 @@
 import jax 
 import dLux as dl
+import equinox as eqx
 import jax.numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
 jax.config.update("jax_enable_x64", True)
+
+
+class NicmosColdMask(dl.CompoundAperture):
+    x_offset: float
+    y_offset: float
+        
+    def __init__(self, x_offset: float, y_offset: float):
+        self.x_offset = np.asarray(x_offset).astype(float)
+        self.y_offset = np.asarray(y_offset).astype(float)
+        self.apertures = {
+            "Outer": dl.CircularAperture(
+                x_offset = x_offset,
+                y_offset = y_offset,
+                radius = 1.2,
+                occulting = False,
+                softening = True),
+            "Obstruction": dl.CircularAperture(
+                x_offset = x_offset,
+                y_offset = y_offset,
+                radius = 0.4464,
+                occulting = True,
+                softening = True),
+            "Spider": dl.EvenUniformSpider(
+                x_offset = x_offset,
+                y_offset = y_offset,
+                number_of_struts = 4,
+                width_of_struts = 0.0402,
+                rotation = 0.785398163,
+                softening = True),
+            "Mirror Pad 1": dl.SquareAperture(
+                x_offset = 1.070652 * np.cos(- np.pi / 4) + x_offset, 
+                y_offset = 1.070652 * np.sin(- np.pi / 4) + y_offset,
+                theta = - np.pi / 4,
+                width = 0.156,
+                occulting = True,
+                softening = True),
+            "Mirror Pad 2": dl.SquareAperture(
+                x_offset = 1.070652 * np.cos(- np.pi / 4 + 2 * np.pi / 3) + x_offset, 
+                y_offset = 1.070652 * np.sin(- np.pi / 4 + 2 * np.pi / 3) + y_offset,
+                theta = - np.pi / 4 + np.pi / 3,
+                width = 0.156,
+                occulting = True, 
+                softening = True),
+            "Mirror Pad 3": dl.SquareAperture(
+                x_offset = 1.070652 * np.cos(- np.pi / 4 - 2 * np.pi / 3) + x_offset, 
+                y_offset = 1.070652 * np.sin(- np.pi / 4 - 2 * np.pi / 3) + y_offset,
+                theta = - np.pi / 3 - np.pi / 4,
+                width = 0.156,
+                occulting = True,
+                softening = True)}
+        
+        
+    def set_offset(self, delta_x_offset: float, delta_y_offset: float):
+        for aperture in self.apertures:
+            self[aperture] = self[aperture]\
+                .set_x_offset(self[aperture].get_x_offset() + delta_x_offset)\
+                .set_y_offset(self[aperture].get_y_offset() + delta_y_offset)
+        
+        return eqx.tree_at(
+            lambda aperture: (aperture.x_offset, aperture.y_offset), 
+                self, (np.asarray(self.x_offset + delta_x_offset).astype(float), 
+                    np.asarray(self.y_offset + delta_y_offset).astype(float)))
+    
+
+nicmos = NicmosColdMask(- 0.0678822510,  - 0.0678822510)
+
+nicmos = nicmos.set_offset(-0.1, 0.3)
+
+coordinates = dl.utils.get_pixel_coordinates(1024, 0.003, 0, 0)
+plt.imshow(nicmos._aperture(coordinates))
+plt.colorbar()
+plt.show()
 
 apertures = {
     "1": dl.CircularAperture( 
@@ -45,46 +118,7 @@ apertures = {
         rotation = 0.785398163,
         softening = True),
     # NOTE: Below this is the cold mask
-    "12": dl.CircularAperture(
-        x_offset = - 0.0678822510,
-        y_offset = - 0.0678822510,
-        radius = 1.2,
-        occulting = False,
-        softening = True),
-    "6": dl.CircularAperture(
-        x_offset = - 0.0678822510,
-        y_offset = - 0.0678822510,
-        radius = 0.4464,
-        occulting = True,
-        softening = True),
-    "7": dl.EvenUniformSpider(
-        x_offset = - 0.0678822510,
-        y_offset = - 0.0678822510,
-        number_of_struts = 4,
-        width_of_struts = 0.0402,
-        rotation = 0.785398163,
-        softening = True),
-    "8": dl.SquareAperture(
-        x_offset = 1.070652 * np.cos(- np.pi / 4) - 0.0678822510, 
-        y_offset = 1.070652 * np.sin(- np.pi / 4) - 0.0678822510,
-        theta = - np.pi / 4,
-        width = 0.156,
-        occulting = True,
-        softening = True),
-    "9": dl.SquareAperture(
-        x_offset = 1.070652 * np.cos(- np.pi / 4 + 2 * np.pi / 3) - 0.0678822510, 
-        y_offset = 1.070652 * np.sin(- np.pi / 4 + 2 * np.pi / 3) - 0.0678822510,
-        theta = - np.pi / 4 + np.pi / 3,
-        width = 0.156,
-        occulting = True, 
-        softening = True),
-    "10": dl.SquareAperture(
-        x_offset = 1.070652 * np.cos(- np.pi / 4 - 2 * np.pi / 3) - 0.0678822510, 
-        y_offset = 1.070652 * np.sin(- np.pi / 4 - 2 * np.pi / 3) - 0.0678822510,
-        theta = - np.pi / 3 - np.pi / 4,
-        width = 0.156,
-        occulting = True,
-        softening = True)}
+    
 
 file_name = "data/MAST_2022-08-02T2026/HST/n9nk01010/n9nk01010_mos.fits"
 
