3765e14ea87bba22ebbe3afad955e8b36f280a28
Jordan Dennis <jdenn105@gmail.com>
2022-09-28 07:18:32 +1000

    Working optimisation on simulated data.

--- hubble.py
+++ hubble.py
@@ -120,58 +120,51 @@ class HubblePupil(dl.CompoundAperture):
                 softening = True)}
 
 
-dlux_aperture = dl.CompoundAperture({
-    "Pupil": HubblePupil(),
-    "Nicmos": NicmosColdMask(-0.06788225 / 2., 0.06788225 / 2.)})
-
-file_name = "data/MAST_2022-08-02T2026/HST/n9nk01010/n9nk01010_mos.fits"
-
-with fits.open(file_name) as hubble_data:
-    data = np.array(hubble_data[1].data[46:110, 185:249])
-
 with open("data/filters/HST_NICMOS1.F170M.dat") as filter_data:
+    next(filter_data)
     nicmos_filter = np.array(
         [[float(entry) for entry in line.strip().split(" ")] for line in filter_data])
 
 
-basis = dl.utils.zernike_basis(5, 512, outside=0.)
-coeffs = 2e-8*jax.random.normal(jax.random.PRNGKey(0), [len(basis)])
+
+
+basis = dl.utils.zernike_basis(5, 256, outside=0.)
+target_coeffs = 1e-7 * jax.random.normal(jax.random.PRNGKey(0), [len(basis)])
+initial_coeffs = 1e-7 * jax.random.normal(jax.random.PRNGKey(0), [len(basis)])
 
 hubble = dl.OpticalSystem(
-    [dl.CreateWavefront(512, 2.4, wavefront_type='Angular'), 
-     dlux_aperture,
+    [dl.CreateWavefront(256, 2.4, wavefront_type='Angular'), 
+     dl.CompoundAperture({"Hubble": HubblePupil(), 
+                          "Nicmos": NicmosColdMask(-0.06788225 / 2., 0.06788225 / 2.)}),
      dl.NormaliseWavefront(),
-     dl.ApplyBasisOPD(basis, coeffs),
+     dl.ApplyBasisOPD(basis, target_coeffs),
      dl.AngularMFT(dl.utils.arcsec2rad(0.043), 64)], 
     wavels = nicmos_filter[:, 0] * 1e-9, 
     weights = nicmos_filter[:, 1])
 
-data = (data) / data.sum()
-
 # +
-psf = hubble.propagate()
-plt.figure(figsize=(10, 10))
-plt.subplot(2, 2, 1)
-plt.title("Linear scale")
-plt.imshow(psf)
-plt.colorbar()
-
-plt.subplot(2, 2, 2)
+target_psf = hubble.propagate()
+plt.figure(figsize=(8, 3))
+plt.subplot(1, 2, 1)
 plt.title("Log scale")
-plt.imshow(psf ** 0.25)
+plt.imshow(target_psf ** 0.25)
 plt.colorbar()
 
-plt.subplot(2, 2, 3)
-plt.title("Linear scale")
-plt.imshow(data)
-plt.colorbar()
-
-plt.subplot(2, 2, 4)
-plt.title("Log scale")
-plt.imshow((data / data.max()) ** 0.25)
+plt.subplot(1, 2, 2)
+plt.title("Input Aperture")
+plt.imshow(hubble.layers[1]._aperture(dl.utils.get_pixel_coordinates(1024, 0.003, 0., 0.)))
 plt.colorbar()
 plt.show()
 # -
+
+hubble = dl.OpticalSystem(
+    [dl.CreateWavefront(256, 2.4, wavefront_type='Angular'), 
+     dl.CompoundAperture({"Hubble": HubblePupil(), "Nicmos": NicmosColdMask(0., 0.)}),
+     dl.NormaliseWavefront(),
+     dl.ApplyBasisOPD(basis, initial_coeffs),
+     dl.AngularMFT(dl.utils.arcsec2rad(0.043), 64)], 
+    wavels = nicmos_filter[:, 0] * 1e-9, 
+    weights = nicmos_filter[:, 1])
 
 filter_spec = eqx.tree_at(lambda tree: 
         (tree.layers[1]["Nicmos"].delta_x_offset, 
@@ -182,12 +175,12 @@ filter_spec = eqx.tree_at(lambda tree:
 
 @eqx.filter_jit
 @eqx.filter_value_and_grad(arg=filter_spec)
-def loss_func(model, data):
+def loss_func(model, target_psf):
     out = model.propagate()
-    return np.sum((data - out) ** 2)
+    return np.sum((target_psf - out) ** 2)
 
 
-loss, grads = loss_func(hubble, data)
+loss, grads = loss_func(hubble, target_psf)
 
 # +
 optim = optax.adam(1e-1)
@@ -195,19 +188,17 @@ opt_state = optim.init(hubble)
 
 errors, grads_out, models_out = [], [], []
 
-with tqdm.tqdm(range(5), desc='Gradient Descent') as t:
+with tqdm.tqdm(range(10), desc='Gradient Descent') as t:
     for i in t: 
-        loss, grads = loss_func(hubble, data)
+        loss, grads = loss_func(hubble, target_psf)
         updates, opt_state = optim.update(grads, opt_state)
         
         delta_y_offset = updates\
-            .layers[1]\
-            .apertures["Nicmos"]\
+            .layers[1]["Nicmos"]\
             .delta_y_offset
         
         delta_x_offset = updates\
-            .layers[1]\
-            .apertures["Nicmos"]\
+            .layers[1]["Nicmos"]\
             .delta_x_offset
         
         nicmos = hubble.layers[1]["Nicmos"]
@@ -225,25 +216,20 @@ with tqdm.tqdm(range(5), desc='Gradient Descent') as t:
 
 # +
 psf = models_out[-1].propagate()
-plt.figure(figsize=(10, 10))
-plt.subplot(2, 2, 1)
-plt.title("Linear scale")
-plt.imshow(psf)
-plt.colorbar()
-
-plt.subplot(2, 2, 2)
+plt.figure(figsize=(12, 3))
+plt.subplot(1, 3, 1)
 plt.title("Log scale")
 plt.imshow(psf ** 0.25)
 plt.colorbar()
 
-plt.subplot(2, 2, 3)
-plt.title("Linear scale")
-plt.imshow(data)
+plt.subplot(1, 3, 2)
+plt.title("Log scale")
+plt.imshow(target_psf ** 0.25)
 plt.colorbar()
 
-plt.subplot(2, 2, 4)
-plt.title("Log scale")
-plt.imshow(data ** 0.25)
+plt.subplot(1, 3, 3)
+plt.title("Residuals")
+plt.imshow((psf - target_psf))
 plt.colorbar()
 plt.show()
 # -
@@ -253,3 +239,5 @@ aperture = models_out[-1].layers[1]._aperture(dl.utils.get_pixel_coordinates(102
 plt.imshow(aperture)
 plt.colorbar()
 plt.show()
+
+
